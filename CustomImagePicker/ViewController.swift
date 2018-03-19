c1890562ec6c87a0cfa8a0304af1c57090d9a3ae
lim-james <jameslimbj@gmail.com>
2018-03-19 10:43:19 +0800

    Initial Commit

---
+++ CustomImagePicker/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CustomImagePicker
+//
+//  Created by James on 19/3/18.
+//  Copyright © 2018 james. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
